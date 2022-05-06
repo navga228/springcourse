06c5409dd7afad171d59fab600d7d7ead72af7d2
navga228 <novgorodov74@bk.ru>
2022-05-06 19:46:15 +0300

    Урок 6: Внедрение зависимостей с помощью геттеров. Внедрение числовых зависимостей. Внедрение числовых зависимостей из отдельного файла.

--- src/main/java/ru/navga228/springcourse/MusicPlayer.java
+++ src/main/java/ru/navga228/springcourse/MusicPlayer.java
@@ -2,10 +2,35 @@ package ru.navga228.springcourse;
 
 public class MusicPlayer {
     private Music music;
+    private String name;
+    private int volume;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
 
     public MusicPlayer(Music music) {
         this.music = music;
     }
+
+    public MusicPlayer(){} // Пустой конструктор нужен тк когда мы создаем свой собственный конструктор, то конструктор по умолчанию удаляется
+
+    public void setMusic(Music music) {
+        this.music = music;
+    }
+
 
     public void playMusic() {
         System.out.println("Playing: " + music.GetMusic());
