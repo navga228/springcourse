5b25f684e66b4c6225b0881dda470275ad4ab95a
navga228 <novgorodov74@bk.ru>
2022-05-07 21:43:44 +0300

    Урок 8: Жизненный цикл бина (Bean Lifecycle). Init, Destroy и Factory методы.

--- src/main/java/ru/navga228/springcourse/ClassicalMusic.java
+++ src/main/java/ru/navga228/springcourse/ClassicalMusic.java
@@ -1,6 +1,20 @@
 package ru.navga228.springcourse;
 
 public class ClassicalMusic implements Music{
+    private ClassicalMusic(){}
+
+    public static ClassicalMusic getClassicalMusic(){ // Factory-method для создания новго бина
+        return new ClassicalMusic();
+    }
+
+    public void doMyInit(){
+        System.out.println("Doing my initialisation");
+    }
+
+    public void doMyDestroy(){
+        System.out.println("Doing my destruction");
+    }
+
     @Override
     public String GetMusic() {
         return "Symphony No. 40 W.A. Mozart";
--- src/main/java/ru/navga228/springcourse/TestSpring.java
+++ src/main/java/ru/navga228/springcourse/TestSpring.java
@@ -10,12 +10,15 @@ public class TestSpring {
 
 //        Music music = context.getBean("musicBean", Music.class);
 //        MusicPlayer musicPlayer = new MusicPlayer(music);
+//
+//        MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//        MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
 
-        MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        ClassicalMusic firstClassicalMusic = context.getBean("musicBean", ClassicalMusic.class);
+        System.out.println(firstClassicalMusic.GetMusic());
 
-        System.out.println(firstMusicPlayer);
-        System.out.println(secondMusicPlayer);
+        ClassicalMusic secondClassicalMusic = context.getBean("musicBean", ClassicalMusic.class);
+        System.out.println(secondClassicalMusic.GetMusic());
 
         context.close(); // После работы с applicationContex'ом его нужно обязательно закрывать
     }
